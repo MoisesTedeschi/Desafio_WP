da0ecb6414b156ad6d547d78c2311fc7215d7d86
Moises Tedeschi <moisestedeschi@gmail.com>
2017-08-22 01:56:45 -0300

    Chamando o css do projeto Desafio.

--- wp-content/themes/desafiowp/functions.php
+++ wp-content/themes/desafiowp/functions.php
@@ -6,7 +6,16 @@ function carrega_scripts(){
 
 	wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', array(), '3.3.7', 'all');
 
-	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), null, true);	
-	
+	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), null, true);
+
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0', 'all');	
 }
 add_action( 'wp_enqueue_scripts', 'carrega_scripts' );
+
+//Chamando a função de registro de menu
+register_nav_menus( 
+	array(
+		'menu_principal_desafio' => 'Menu Principal',
+		'menu_footer_desafio'    => 'Menu Footer'
+	)
+);
